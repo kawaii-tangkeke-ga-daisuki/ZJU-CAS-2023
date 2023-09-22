49a6d5cfcc684b1fbd1acc69be9b1b0a7f1f5165
William Lai <1265613787@qq.com>
2023-09-22 11:19:29 +0800

    rv32core & hazctrl

--- Lab1/Exp1/code/core/HazardDetectionUnit.v
+++ Lab1/Exp1/code/core/HazardDetectionUnit.v
@@ -15,6 +15,27 @@ module HazardDetectionUnit(
         if (((rs1use_ID) & (rs1_ID != 0) & (rd_MEM == rs1_ID)) | ((rs2use_ID) & (rs2_ID != 0) & (rd_MEM == rs2_ID))) begin
             // mem haz
             PC_EN_IF = 1'b0;
+            reg_FD_EN = 1'b0;
+            reg_FD_flush = 1'b1;
+            reg_DE_EN <= 1'b0;
+            reg_DE_flush <= 1'b1;
+            reg_EM_EN <= 1'b0;
+            reg_EM_flush <= 1'b1;
+            reg_MW_EN <= 1'b0;
+            reg_FD_stall <= 1'b1;
+
+            forward_ctrl_ls <= 1'b1;
+
+            if rs1use_ID begin
+                forward_ctrl_A <= 2'b11;
+            end else if rs2use_ID begin
+                forward_ctrl_B <= 2'b11;
+            end
+
+        end else if(((rs1use_ID) & (rs1_ID != 0) & (rd_EXE == rs1_ID)) | ((rs2use_ID) & (rs2_ID != 0) & (rd_EXE == rs2_ID))) begin
+            // ex haz
+            PC_EN_IF = 1'b0;
+            reg_FD_EN = 1'b;
             reg_FD_flush = 1'b1;
             reg_DE_EN <= 0;
             reg_DE_flush <= 1'b1;
@@ -23,23 +44,47 @@ module HazardDetectionUnit(
             reg_MW_EN <= 0;
             reg_FD_stall <= 1'b1;
 
-        end else if(rs2use_ID & ((rs2_ID == rd_EXE)|(rs2_ID == rd_MEM))) begin
-            
-        end else if(Branch_ID & ())
+            forward_ctrl_ls <= 1'b0;
+
+            if rs1use_ID begin
+                forward_ctrl_A <= 2'b10;
+            end else if rs2use_ID begin
+                forward_ctrl_B <= 2'b10;
+            end
+        end else if(Branch_ID) begin
+            // ctrl haz
+            PC_EN_IF = 1'b0;
+            reg_FD_EN = 1'b;
+            reg_FD_flush = 1'b1;
+            reg_DE_EN <= 0;
+            reg_DE_flush <= 1'b1;
+            reg_EM_EN <= 0;
+            reg_EM_flush <= 1'b1;
+            reg_MW_EN <= 0;
+            reg_FD_stall <= 1'b1;
+
+            forward_ctrl_ls <= 1'b0;
+
+            if rs1use_ID begin
+                forward_ctrl_A <= 2'b10;
+            end else if rs2use_ID begin
+                forward_ctrl_B <= 2'b10;
+            end
+        end else begin
+            PC_EN_IF = 1'b0;
+            reg_FD_EN = 1'b;
+            reg_FD_flush = 1'b1;
+            reg_DE_EN <= 0;
+            reg_DE_flush <= 1'b1;
+            reg_EM_EN <= 0;
+            reg_EM_flush <= 1'b1;
+            reg_MW_EN <= 0;
+            reg_FD_stall <= 1'b1;
+
+            forward_ctrl_ls <= 1'b0;
+            forward_ctrl_A <= 2'b00;
+            forward_ctrl_B <= 2'b00;
+        end
     end        //according to the diagram, design the Hazard Detection Unit
-
-    if  begin
-        // MEM hazard
-        reg_FD_stall = 1'b1;
-    end
-    if (((rs1use_ID) & (rs1_ID != 0) & (rd_EXE == rs1_ID)) | ((rs2use_ID) & (rs2_ID != 0) & (rd_EXE == rs2_ID))) begin
-        // EX hazard
-        
-    end
-    if Branch_ID begin
-        // ctrl hazard
-
-    end
-
 
 endmodule
